b6978a543c96cc953df974b3d6afc7c748fae61d
charles francis <charlsefrancis112@gmail.com>
2020-06-21 18:50:18 +0800

    Add files via upload

---
+++ build/precache-manifest.3e7a36248cfd41c05db34d4a05a0ac3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f294382b5b427a72236397bf3a97b6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cfc0387878887bf4927",
+    "url": "/static/css/main.5f3810cc.chunk.css"
+  },
+  {
+    "revision": "16c4a0e30d4704f38437",
+    "url": "/static/js/2.75993b1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75993b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfc0387878887bf4927",
+    "url": "/static/js/main.e6ebfb3e.chunk.js"
+  },
+  {
+    "revision": "472e72063ec198abe343",
+    "url": "/static/js/runtime-main.5afefb00.js"
+  },
+  {
+    "revision": "f49f54e428c6cc85b74409aa68254fb1",
+    "url": "/static/media/black.f49f54e4.png"
+  },
+  {
+    "revision": "182fd0442a5ee9c468c3695506fc8d69",
+    "url": "/static/media/blue.182fd044.png"
+  },
+  {
+    "revision": "88f61bc52478d5190ee00d7b81198ef3",
+    "url": "/static/media/green.88f61bc5.png"
+  },
+  {
+    "revision": "b3434e3e703dc1cefa6052ace31234c1",
+    "url": "/static/media/logo.b3434e3e.png"
+  },
+  {
+    "revision": "0e59d7df6292539663350c2ccb7b3cc2",
+    "url": "/static/media/orange.0e59d7df.png"
+  },
+  {
+    "revision": "60bfc4f39a5c7b5a6ac4b9769ebed74b",
+    "url": "/static/media/red.60bfc4f3.png"
+  }
+]);
